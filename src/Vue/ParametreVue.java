574f02db5e89a91d94bfcfe2a56552f742c26259
Léo Letourneur <letourneur.leo@gmail.com>
2015-05-27 21:26:00 +0200

    sprite / parametre

---
+++ src/Vue/ParametreVue.java
@@ -0,0 +1,70 @@
+package Vue;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+
+public class ParametreVue extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ParametreVue dialog = new ParametreVue();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ParametreVue() {
+		setBounds(100, 100, 460, 322);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+			tabbedPane.setBounds(6, 10, 448, 245);
+			contentPanel.add(tabbedPane);
+			
+			JPanel panelPartie = new JPanel();
+			tabbedPane.addTab("Partie", null, panelPartie, null);
+			
+			JPanel panelGrille = new JPanel();
+			tabbedPane.addTab("Grille", null, panelGrille, null);
+			{
+				JPanel panelTheme = new JPanel();
+				tabbedPane.addTab("Thème", null, panelTheme, null);
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
