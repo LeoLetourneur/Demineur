41f0272009dd2d5a6f50807b5aa22f64e40d1f63
Léo Letourneur <letourneur.leo@gmail.com>
2015-06-14 23:11:04 +0200

    Point de style

--- src/Vue/ScoreVue.java
+++ src/Vue/ScoreVue.java
@@ -9,6 +9,10 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 import Modele.Partie;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class ScoreVue extends JDialog {
 	private static final long serialVersionUID = 8874069533716005260L;
@@ -17,7 +21,7 @@ public class ScoreVue extends JDialog {
 	public JTabbedPane tabbedPane;
 	
 	public ScoreVue() {
-		setSize(450, 300);
+		setSize(450, 360);
 		setResizable(false);
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setTitle("Scores");
@@ -56,6 +60,41 @@ public class ScoreVue extends JDialog {
 		table2J = new JTable(tableModel);
 		table2J.setBounds(6, 6, 405, 208);
 		panel2J.add(table2J);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(16, 268, 415, 64);
+		getContentPane().add(panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{208, 0, 208, 0};
+		gbl_panel.rowHeights = new int[]{32, 32, 0};
+		gbl_panel.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblCouturierCyril = new JLabel("COUTURIER Cyril");
+		GridBagConstraints gbc_lblCouturierCyril = new GridBagConstraints();
+		gbc_lblCouturierCyril.fill = GridBagConstraints.BOTH;
+		gbc_lblCouturierCyril.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCouturierCyril.gridx = 0;
+		gbc_lblCouturierCyril.gridy = 0;
+		panel.add(lblCouturierCyril, gbc_lblCouturierCyril);
+		
+		JLabel lblLetourneurLo = new JLabel("LETOURNEUR Léo");
+		GridBagConstraints gbc_lblLetourneurLo = new GridBagConstraints();
+		gbc_lblLetourneurLo.anchor = GridBagConstraints.EAST;
+		gbc_lblLetourneurLo.fill = GridBagConstraints.VERTICAL;
+		gbc_lblLetourneurLo.insets = new Insets(0, 0, 5, 0);
+		gbc_lblLetourneurLo.gridx = 2;
+		gbc_lblLetourneurLo.gridy = 0;
+		panel.add(lblLetourneurLo, gbc_lblLetourneurLo);
+		
+		JLabel lblNewLabel = new JLabel("Polytech Lyon ©");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 1;
+		panel.add(lblNewLabel, gbc_lblNewLabel);
 		
 	}
 }
